087f3279429cacfc50fa49f97786c82bc44696e6
Yemre <aybey77@gmail.com>
2019-12-03 20:38:40 +0300

    Updates

---
+++ precache-manifest.028ec2414d4dfa0ad227363bd886ef36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcff0923505123e46a3b716d374de857",
+    "url": "/courses/index.html"
+  },
+  {
+    "revision": "5727be8276d5edb1a270",
+    "url": "/courses/static/css/main.a91b271d.chunk.css"
+  },
+  {
+    "revision": "a9d5ef0388cfcdf4f65a",
+    "url": "/courses/static/js/2.5563ef0c.chunk.js"
+  },
+  {
+    "revision": "5727be8276d5edb1a270",
+    "url": "/courses/static/js/main.8c8bfadc.chunk.js"
+  },
+  {
+    "revision": "c051715e62fe25a64735",
+    "url": "/courses/static/js/runtime-main.2c81a11f.js"
+  },
+  {
+    "revision": "8614e1a4b4b46891877d6ad39768d6b6",
+    "url": "/courses/static/media/rails-logo.8614e1a4.png"
+  },
+  {
+    "revision": "83fb4e935ec43deec7bdadc5d3620cbf",
+    "url": "/courses/static/media/react-logo.83fb4e93.png"
+  },
+  {
+    "revision": "9beaf80c5aff9d345b841695c653fb69",
+    "url": "/courses/static/media/ruby-logo.9beaf80c.png"
+  }
+]);
